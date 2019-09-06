929ccfc5e22ce3234e51ff0a96c5560e3e7a4b7f
EJ <ejg132@gmail.com>
2019-09-06 13:40:43 -0700

    done

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,10 @@ import './Card.css';
 import './index.css';
 
 function Card(props) {
+
   return (
     <div className='Card' key={props.id}>
-      <button type='button'> delete </button>
+      <button onClick={() => props.delete(props.id)} type='button'> delete </button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
     </div>
